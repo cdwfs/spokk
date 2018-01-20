b1be1c651f9b6d5859d3417378fd58e84957574e
Cort Stratton <cdwfs@users.noreply.github.com>
2018-01-19 16:19:55 -0800

    First pass at optimizing ComposeTransform()

--- src/spokk/spokk_math.cpp
+++ src/spokk/spokk_math.cpp
@@ -9,16 +9,36 @@
 namespace spokk {
 
 glm::mat4 ComposeTransform(const glm::vec3 translation, const glm::quat rotation, const float uniform_scale) {
-  // clang-format off
-  return glm::scale(
-    glm::rotate(
-      glm::translate(
-        glm::mat4(1.0f),
-        translation),
-      glm::angle(rotation),
-      glm::axis(rotation)),
-    glm::vec3(uniform_scale));
-  // clang-format on
+  float qxx(rotation.x * rotation.x);
+  float qyy(rotation.y * rotation.y);
+  float qzz(rotation.z * rotation.z);
+  float qxz(rotation.x * rotation.z);
+  float qxy(rotation.x * rotation.y);
+  float qyz(rotation.y * rotation.z);
+  float qwx(rotation.w * rotation.x);
+  float qwy(rotation.w * rotation.y);
+  float qwz(rotation.w * rotation.z);
+  glm::mat4 xform = glm::mat4(
+      // clang-format off
+      (1.0f - 2.0f * (qyy +  qzz)) * uniform_scale,
+      (2.0f * (qxy + qwz)) * uniform_scale,
+      (2.0f * (qxz - qwy)) * uniform_scale,
+      0,
+
+      (2.0f * (qxy - qwz)) * uniform_scale,
+      (1.0f - 2.0f * (qxx +  qzz)) * uniform_scale,
+      (2.0f * (qyz + qwx)) * uniform_scale,
+      0,
+
+      (2.0f * (qxz + qwy)) * uniform_scale,
+      (2.0f * (qyz - qwx)) * uniform_scale,
+      (1.0f - 2.0f * (qxx +  qyy)) * uniform_scale,
+      0,
+
+      translation.x, translation.y, translation.z, 1
+      // clang-format on
+  );
+  return xform;
 }
 
 #if defined(ZOMBO_COMPILER_MSVC)
@@ -26,7 +46,7 @@ glm::mat4 ComposeTransform(const glm::vec3 translation, const glm::quat rotation
 #endif
 glm::vec3 ExtractViewPos(const glm::mat4& view) {
   glm::mat3 view_rot(
-    view[0][0], view[0][1], view[0][2], view[1][0], view[1][1], view[1][2], view[2][0], view[2][1], view[2][2]);
+      view[0][0], view[0][1], view[0][2], view[1][0], view[1][1], view[1][2], view[2][0], view[2][1], view[2][2]);
   glm::vec3 d(view[3][0], view[3][1], view[3][2]);
   return -d * view_rot;
 }
@@ -34,8 +54,6 @@ glm::vec3 ExtractViewPos(const glm::mat4& view) {
 #pragma float_control(pop)
 #endif
 
-glm::vec3 ExtractViewDir(const glm::mat4& view) {
-  return glm::vec3(-view[0][2], -view[1][2], -view[2][2]);
-}
+glm::vec3 ExtractViewDir(const glm::mat4& view) { return glm::vec3(-view[0][2], -view[1][2], -view[2][2]); }
 
 }  // namespace spokk
